bfc34efceb5702ca0212b64f439a0bd8f687b1ba
andreaortogni <andreaortogni@gmail.com>
2023-02-02 11:46:22 +0100

    removed test code

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -9,13 +9,6 @@ use stdClass;
 class HomeController extends Controller {
     
     public function create() {
-        $levels = [
-            "Jlpt5",
-            "Jlpt4",
-            "Jlpt3",
-            "Jlpt2"
-        ];
-
         $levels = Level::getAllLevels();
 
         return Inertia::render("Welcome", [
